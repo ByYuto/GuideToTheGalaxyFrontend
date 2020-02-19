2d9b9486b446288697e29dc137ac249c2a91c472
Pipe <felipebotero@gmail.com>
2020-02-19 15:01:29 -0500

    Code style fixes

--- src/views/About.js
+++ src/views/About.js
@@ -1,16 +1,18 @@
-import React, { useState } from 'react';
-import { incrementCounter } from '../redux/reducers/counter';
-import { useDispatch, useSelector } from 'react-redux';
+import React from "react";
+import { incrementCounter } from "../redux/reducers/counter";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function About() {
-    const counter = useSelector(state => state.counter.counter);
-    const dispatch = useDispatch();
-    function onButtonClick() {
-        dispatch(incrementCounter());
-    }
-    return <div><h2>About</h2>
-        <p>El contador va en {counter}</p>
-        <button onClick={onButtonClick}>Press me</button>
-
+  const counter = useSelector(state => state.counter.counter);
+  const dispatch = useDispatch();
+  function onButtonClick() {
+    dispatch(incrementCounter());
+  }
+  return (
+    <div>
+      <h2>About</h2>
+      <p>Counter goes to {counter}</p>
+      <button onClick={onButtonClick}>Press me</button>
     </div>
+  );
 }
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { useSelector } from "react-redux";
 export default function Home() {
-    const counter = useSelector(state => state.counter.counter);
-    return <div>
-        <h2>Home</h2>
-        <p>El contador va en {counter}</p>
+  const counter = useSelector(state => state.counter.counter);
+  return (
+    <div>
+      <h2>Home</h2>
+      <p>Counter stops at {counter}</p>
     </div>
+  );
 }
