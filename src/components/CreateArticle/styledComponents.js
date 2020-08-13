7a5860d0fa777ed10df042fb29361ba68c2345ff
Abraham Sanchez <sanchezbaj@gmail.com>
2020-08-13 12:44:01 -0400

    fix style in toolbar

--- src/components/CreateArticle/styledComponents.js
+++ src/components/CreateArticle/styledComponents.js
@@ -3,11 +3,11 @@ import styled from 'styled-components';
 export const DivInputRow = styled.div`
   display: flex;
   align-items: center;
-  justifycontent: space-between;
+  justify-content: space-between;
 `;
 
 export const DivInputColumn = styled.div`
   display: flex;
   alignitems: center;
-  justifycontent: space-between;
+  justify-content: space-between;
 `;
